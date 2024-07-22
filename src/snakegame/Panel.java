18cf06a07e3f8a40bfa9dd66a89ba75a6f02fedd
Gabriel Piske <156837794+gabrielpiske@users.noreply.github.com>
2024-07-22 10:44:28 -0300

    Update Panel.java

--- src/snakegame/Panel.java
+++ src/snakegame/Panel.java
@@ -1,9 +1,9 @@
 package snakegame;
 
-import java.awt.Graphics;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.JPanel;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
 
 /**
  *
@@ -11,40 +11,179 @@ import javax.swing.JPanel;
  */
 public class Panel extends JPanel implements ActionListener {
 
-    Panel() {
+    static final int WIDTH_JANELA = 600;
+    static final int HEIGHT_JANELA = 600;
+    static final int UNIT_SIZE = 25;
+    static final int GAME_UNITS = (WIDTH_JANELA * HEIGHT_JANELA) / UNIT_SIZE;
+    static final int DELAY = 75;
+    final int x[] = new int[GAME_UNITS];
+    final int y[] = new int[GAME_UNITS];
+    int bodyParts = 6;
+    int applesEaten;
+    int appleX;
+    int appleY;
+    char direction = 'R';
+    boolean running = false;
+    Timer timer;
+    Random random;
 
+    Panel() {
+        random = new Random();
+        this.setPreferredSize(new Dimension(WIDTH_JANELA, HEIGHT_JANELA));
+        this.setBackground(Color.black);
+        this.setFocusable(true);
+        this.addKeyListener(new MyKeyAdapter());
+        startJogo();
     }
 
     public void startJogo() {
-
+        newApple();
+        running = true;
+        timer = new Timer(DELAY, this);
+        timer.start();
     }
 
-    public void paintComponents(Graphics g) {
-
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
     }
 
     public void draw(Graphics g) {
-        
-    }
-    
-    public void move(){
-        
+        if (running) {
+            for (int i = 0; i < HEIGHT_JANELA / UNIT_SIZE; i++) {
+                g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, HEIGHT_JANELA);
+                g.drawLine(0, i * UNIT_SIZE, WIDTH_JANELA, i * UNIT_SIZE);
+            }
+            g.setColor(Color.red);
+            g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+
+            for (int i = 0; i < bodyParts; i++) {
+                if (i == 0) {
+                    g.setColor(Color.green);
+                    g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+                } else {
+                    g.setColor(new Color(45, 180, 0));
+                    g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+                }
+            }
+            //Score
+            g.setColor(Color.red);
+            g.setFont(new Font("JetBrains Mono", Font.PLAIN, 40));
+            FontMetrics metrics1 = getFontMetrics(g.getFont());
+            g.drawString("Score: " + applesEaten, (WIDTH_JANELA - metrics1.stringWidth("Score: " + applesEaten)) / 2, g.getFont().getSize());
+        } else {
+            gameOver(g);
+        }
     }
 
-    public void checkMaca(){
-        
+    public void newApple() {
+        appleX = random.nextInt((int) (WIDTH_JANELA / UNIT_SIZE)) * UNIT_SIZE;
+        appleY = random.nextInt((int) (HEIGHT_JANELA / UNIT_SIZE)) * UNIT_SIZE;
     }
-    
-    public void checkColisao(){
-        
+
+    public void move() {
+        for (int i = bodyParts; i > 0; i--) {
+            x[i] = x[i - 1];
+            y[i] = y[i - 1];
+        }
+        switch (direction) {
+            case 'U':
+                y[0] = y[0] - UNIT_SIZE;
+                break;
+            case 'D':
+                y[0] = y[0] + UNIT_SIZE;
+                break;
+            case 'L':
+                x[0] = x[0] - UNIT_SIZE;
+                break;
+            case 'R':
+                x[0] = x[0] + UNIT_SIZE;
+                break;
+        }
     }
-    
-    public void gameOver(Graphics g){
-        
+
+    public void checkMaca() {
+        if ((x[0] == appleX) && (y[0] == appleY)) {
+            bodyParts++;
+            applesEaten++;
+            newApple();
+        }
     }
+
+    public void checkColisao() {
+        //cabeça encostando no corpo
+        for (int i = bodyParts; i > 0; i--) {
+            if ((x[0] == x[i]) && (y[0] == y[i])) {
+                running = false;
+            }
+        }
+        //tocando na borda esquerda
+        if (x[0] < 0) {
+            running = false;
+        }
+        //tocando na borda direita
+        if (x[0] > WIDTH_JANELA) {
+            running = false;
+        }
+        //tocando na borda de cima
+        if (y[0] < 0) {
+            running = false;
+        }
+        //tocando na borda de baixo
+        if (y[0] > HEIGHT_JANELA) {
+            running = false;
+        }
+
+        if (!running) {
+            timer.stop();
+        }
+    }
+
+    public void gameOver(Graphics g) {
+        //Tela Game Over
+        g.setColor(Color.red);
+        g.setFont(new Font("JetBrains Mono", Font.PLAIN, 75));
+        FontMetrics metrics2 = getFontMetrics(g.getFont());
+        g.drawString("Game Over", (WIDTH_JANELA - metrics2.stringWidth("Game Over")) / 2, HEIGHT_JANELA / 2);
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
+        if (running) {
+            move();
+            checkMaca();
+            checkColisao();
+        }
+        repaint();
+    }
 
+    public class MyKeyAdapter extends KeyAdapter {
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            switch (e.getKeyCode()) {
+                case KeyEvent.VK_LEFT:
+                    if (direction != 'R') {
+                        direction = 'L';
+                    }
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    if (direction != 'L') {
+                        direction = 'R';
+                    }
+                    break;
+                case KeyEvent.VK_UP:
+                    if (direction != 'D') {
+                        direction = 'U';
+                    }
+                    break;
+                case KeyEvent.VK_DOWN:
+                    if (direction != 'U') {
+                        direction = 'D';
+                    }
+                    break;
+            }
+        }
     }
 
 }
