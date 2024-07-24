d34d7cd04bd11425571566a07c47a906454ede16
Gabriel Piske <156837794+gabrielpiske@users.noreply.github.com>
2024-07-24 13:59:43 -0300

    Update Panel.java

--- src/snakegame/Panel.java
+++ src/snakegame/Panel.java
@@ -11,25 +11,25 @@ import java.util.Random;
  */
 public class Panel extends JPanel implements ActionListener {
 
-    static final int WIDTH_JANELA = 600;
-    static final int HEIGHT_JANELA = 600;
-    static final int UNIT_SIZE = 25;
-    static final int GAME_UNITS = (WIDTH_JANELA * HEIGHT_JANELA) / UNIT_SIZE;
+    static final int LARGURA_JANELA = 600;
+    static final int ALTURA_JANELA = 600;
+    static final int TAM_QUADRADOS = 25;
+    static final int QUADRADOS = (LARGURA_JANELA * ALTURA_JANELA) / TAM_QUADRADOS;
     static final int DELAY = 75;
-    final int x[] = new int[GAME_UNITS];
-    final int y[] = new int[GAME_UNITS];
+    final int x[] = new int[QUADRADOS];
+    final int y[] = new int[QUADRADOS];
     int bodyParts = 6;
-    int applesEaten;
+    int macasComidas;
     int appleX;
     int appleY;
     char direction = 'R';
-    boolean running = false;
+    boolean rodar = false;
     Timer timer;
     Random random;
 
     Panel() {
         random = new Random();
-        this.setPreferredSize(new Dimension(WIDTH_JANELA, HEIGHT_JANELA));
+        this.setPreferredSize(new Dimension(LARGURA_JANELA, ALTURA_JANELA));
         this.setBackground(Color.black);
         this.setFocusable(true);
         this.addKeyListener(new MyKeyAdapter());
@@ -38,7 +38,7 @@ public class Panel extends JPanel implements ActionListener {
 
     public void startJogo() {
         newApple();
-        running = true;
+        rodar = true;
         timer = new Timer(DELAY, this);
         timer.start();
     }
@@ -49,36 +49,36 @@ public class Panel extends JPanel implements ActionListener {
     }
 
     public void draw(Graphics g) {
-        if (running) {
-            for (int i = 0; i < HEIGHT_JANELA / UNIT_SIZE; i++) {
-                g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, HEIGHT_JANELA);
-                g.drawLine(0, i * UNIT_SIZE, WIDTH_JANELA, i * UNIT_SIZE);
+        if (rodar) {
+            for (int i = 0; i < ALTURA_JANELA / TAM_QUADRADOS; i++) {
+                g.drawLine(i * TAM_QUADRADOS, 0, i * TAM_QUADRADOS, ALTURA_JANELA);
+                g.drawLine(0, i * TAM_QUADRADOS, LARGURA_JANELA, i * TAM_QUADRADOS);
             }
             g.setColor(Color.red);
-            g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+            g.fillOval(appleX, appleY, TAM_QUADRADOS, TAM_QUADRADOS);
 
             for (int i = 0; i < bodyParts; i++) {
                 if (i == 0) {
                     g.setColor(Color.green);
-                    g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+                    g.fillRect(x[i], y[i], TAM_QUADRADOS, TAM_QUADRADOS);
                 } else {
                     g.setColor(new Color(45, 180, 0));
-                    g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+                    g.fillRect(x[i], y[i], TAM_QUADRADOS, TAM_QUADRADOS);
                 }
             }
             //Score
             g.setColor(Color.red);
             g.setFont(new Font("JetBrains Mono", Font.PLAIN, 40));
             FontMetrics metrics1 = getFontMetrics(g.getFont());
-            g.drawString("Score: " + applesEaten, (WIDTH_JANELA - metrics1.stringWidth("Score: " + applesEaten)) / 2, g.getFont().getSize());
+            g.drawString("Score: " + macasComidas, (LARGURA_JANELA - metrics1.stringWidth("Score: " + macasComidas)) / 2, g.getFont().getSize());
         } else {
             gameOver(g);
         }
     }
 
     public void newApple() {
-        appleX = random.nextInt((int) (WIDTH_JANELA / UNIT_SIZE)) * UNIT_SIZE;
-        appleY = random.nextInt((int) (HEIGHT_JANELA / UNIT_SIZE)) * UNIT_SIZE;
+        appleX = random.nextInt((int) (LARGURA_JANELA / TAM_QUADRADOS)) * TAM_QUADRADOS;
+        appleY = random.nextInt((int) (ALTURA_JANELA / TAM_QUADRADOS)) * TAM_QUADRADOS;
     }
 
     public void move() {
@@ -88,16 +88,16 @@ public class Panel extends JPanel implements ActionListener {
         }
         switch (direction) {
             case 'U':
-                y[0] = y[0] - UNIT_SIZE;
+                y[0] = y[0] - TAM_QUADRADOS;
                 break;
             case 'D':
-                y[0] = y[0] + UNIT_SIZE;
+                y[0] = y[0] + TAM_QUADRADOS;
                 break;
             case 'L':
-                x[0] = x[0] - UNIT_SIZE;
+                x[0] = x[0] - TAM_QUADRADOS;
                 break;
             case 'R':
-                x[0] = x[0] + UNIT_SIZE;
+                x[0] = x[0] + TAM_QUADRADOS;
                 break;
         }
     }
@@ -105,7 +105,7 @@ public class Panel extends JPanel implements ActionListener {
     public void checkMaca() {
         if ((x[0] == appleX) && (y[0] == appleY)) {
             bodyParts++;
-            applesEaten++;
+            macasComidas++;
             newApple();
         }
     }
@@ -114,27 +114,27 @@ public class Panel extends JPanel implements ActionListener {
         //cabeça encostando no corpo
         for (int i = bodyParts; i > 0; i--) {
             if ((x[0] == x[i]) && (y[0] == y[i])) {
-                running = false;
+                rodar = false;
             }
         }
         //tocando na borda esquerda
         if (x[0] < 0) {
-            running = false;
+            rodar = false;
         }
         //tocando na borda direita
-        if (x[0] > WIDTH_JANELA) {
-            running = false;
+        if (x[0] > LARGURA_JANELA) {
+            rodar = false;
         }
         //tocando na borda de cima
         if (y[0] < 0) {
-            running = false;
+            rodar = false;
         }
         //tocando na borda de baixo
-        if (y[0] > HEIGHT_JANELA) {
-            running = false;
+        if (y[0] > ALTURA_JANELA) {
+            rodar = false;
         }
 
-        if (!running) {
+        if (!rodar) {
             timer.stop();
         }
     }
@@ -144,12 +144,12 @@ public class Panel extends JPanel implements ActionListener {
         g.setColor(Color.red);
         g.setFont(new Font("JetBrains Mono", Font.PLAIN, 75));
         FontMetrics metrics2 = getFontMetrics(g.getFont());
-        g.drawString("Game Over", (WIDTH_JANELA - metrics2.stringWidth("Game Over")) / 2, HEIGHT_JANELA / 2);
+        g.drawString("Game Over", (LARGURA_JANELA - metrics2.stringWidth("Game Over")) / 2, ALTURA_JANELA / 2);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (running) {
+        if (rodar) {
             move();
             checkMaca();
             checkColisao();
